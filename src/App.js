06c7466c0504ed06875795a79383611ee96593db
Eugene <eugenedatsmee@gmail.com>
2019-10-19 21:30:34 +1300

    updated our menu item to navigate with router Higher or component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
 
 import './pages/homepage/homepage.styles.scss';
 
 import HomePage from './pages/homepage/homepage.component';
 
+const HatsPage =() => (
+  <div>
+    <h1>Hats Page</h1>
+  </div>
+);
+
 function App() {
   return (
-    <div className="App">
-      <HomePage></HomePage>
+    <div>
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route  path='/hats' component={HatsPage} />
+      </Switch>
     </div>
   );
 }
